6fe5d3b0bd63c8aef439ab98e11f53868e7d98a5
bvrongen <93782863+bvrongen@users.noreply.github.com>
2021-12-13 13:58:14 +0100

    Update chapters 3, 4 & 5
    Update chapters 3, 4 & 5

--- r/functions.R
+++ r/functions.R
@@ -44,10 +44,10 @@ statTable <- function(df, parname, rounding, meanorder = "decreasing", sf = F) {
 }
 
 # Plot trends of nutrients
-plotTrends <- function(df, parname, sf = F) {
+plotTrends <- function(df, parname, sf = F, trend = T) {
   
 if(sf) df <- df %>% st_drop_geometry()
-  df %>%
+  p <- df %>%
     dplyr::filter(parametername == parname) %>%
     dplyr::mutate(year = year(datetime), month = month(datetime)) %>%
     dplyr::group_by(stationname, year) %>% 
@@ -60,8 +60,54 @@ if(sf) df <- df %>% st_drop_geometry()
     dplyr::arrange(-Mediaan) %>%
     ggplot(aes(Jaar, Mediaan)) +
     geom_ribbon(aes(ymin = `10-perc`, ymax = `90-perc`), fill = "lightgrey") +
-    geom_line() + geom_point(fill = "white", shape = 21) +
-    geom_smooth(method = "lm", fill = "blue", alpha = 0.2) +
+    geom_line() + geom_point(fill = "white", shape = 21)
+  if(trend)    p <- p + geom_smooth(method = "lm", fill = "blue", alpha = 0.2)
+    p <- p + facet_wrap(~Station) +
+    theme_minimal() +
+    ylab(parname) +
+    coord_cartesian(ylim = c(0,NA))
+    return(p)
+}
+
+plotTrendsWaterstand <- function(df, parname, locname, sf = F) {
+  
+  if(sf) df <- df %>% st_drop_geometry()
+  p <- df %>%
+    dplyr::filter(parametername %in% parname, stationname %in% locname) %>%
+    dplyr::mutate(year = year(datetime), month = month(datetime)) %>%
+    dplyr::group_by(stationname, year, parametername) %>% 
+    dplyr::summarize(mean = mean(value, na.rm = T)) %>%
+    dplyr::select(Station = stationname,
+                  Jaar = year,
+                  Gemiddelde = mean,
+                  Parameter = parametername) %>%
+    dplyr::arrange(-Gemiddelde) %>%
+    ggplot(aes(Jaar, Gemiddelde)) +
+    geom_line(aes(color=Parameter)) + 
+    geom_point(aes(fill=Parameter), color = "white", shape = 21) + 
+    facet_grid(Parameter ~ ., scales="free_y") +
+    theme_minimal() +
+    ylab("Jaargemiddeld hoog- en laagwater in cm+NAP") +
+    coord_cartesian(ylim = c(0,NA))
+  return(p)
+}
+
+plotTrendsBar <- function(df, parname, sf = F) {
+  
+  if(sf) df <- df %>% st_drop_geometry()
+  df %>%
+    dplyr::filter(parametername == parname) %>%
+    dplyr::mutate(year = year(datetime), month = month(datetime)) %>%
+    dplyr::group_by(stationname, year) %>% 
+    dplyr::summarize(median = median(value, na.rm = T), `10-perc` = quantile(value, 0.1, na.rm = T), `90-perc` = quantile(value, 0.9, na.rm = T)) %>%
+    dplyr::select(Station = stationname,
+                  Jaar = year,
+                  Mediaan = median,
+                  `90-perc`,
+                  `10-perc`) %>%
+    dplyr::arrange(-Mediaan) %>%
+    ggplot(aes(x = Jaar, y = Mediaan)) +
+    geom_col() +
     facet_wrap(~Station) +
     theme_minimal() +
     ylab(parname) +
@@ -97,6 +143,29 @@ plotTrendsSeizoen <- function(df, parname, sf = T) {
     coord_cartesian(ylim = c(0,NA))
 }
 
+plotTrendsMaand <- function(df, parname, sf = T) {
+  if(sf) df <- df %>% st_drop_geometry()
+  df %>%
+    filter(parametername == parname) %>%
+    mutate(year = year(datetime), month = month(datetime)) %>%
+    group_by(stationname, year, month) %>% 
+    summarize(median = median(value, na.rm = T), `10-perc` = quantile(value, 0.1, na.rm = T), `90-perc` = quantile(value, 0.9, na.rm = T)) %>%
+    select(Station = stationname,
+           Jaar = year,
+           Mediaan = median,
+           `90-perc`,
+           `10-perc`,
+           Maand = month) %>%
+    arrange(-Mediaan) %>%
+    ggplot(aes(Jaar, Mediaan)) +
+    #geom_ribbon(aes(ymin = `10-perc`, ymax = `90-perc`, fill = month), alpha = 0.4) +
+    geom_line(aes(color = Station)) + #geom_point(aes(color = Station), fill = "white", shape = 21) +
+    # geom_smooth(method = "lm", fill = "blue", alpha = 0.2) +
+    facet_wrap(~Maand) +
+    theme_minimal() +
+    ylab(parname) +
+    coord_cartesian(ylim = c(0,NA))
+}
 
 stationMean <- function(df, parname){
   df %>% #st_drop_geometry() %>%
